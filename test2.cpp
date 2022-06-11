6b3c37ece4304cd38fd0dcf87744bbff103e9906
ps2program <ps2programming@gmail.com>
2022-06-11 22:54:10 +0530

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,2 +1,7 @@
-please write the full code here for the cpp fundamentals and yet there are the cases that you might wanna changes th sequence numbered from the main things inthe 
-dominaitons so that yu can find the remaining things here in the main place for the foundation inthe na memf o
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello world";
+}
