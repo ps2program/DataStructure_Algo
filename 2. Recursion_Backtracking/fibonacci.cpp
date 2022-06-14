cf7bf62f52cbd262bd006ad1d2b01016bb67b148
ps2program <ps2programming@gmail.com>
2022-06-14 20:16:57 +0530

    Create fibonacci.cpp

---
+++ 2. Recursion_Backtracking/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+int Fib(int n) {
+	if(n==0 || n==1)
+		return n;
+	return(Fib(n-1)+Fib(n-2));
+}
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin>>n;
+	cout<<Fib(n);
+	return 0;
+}
